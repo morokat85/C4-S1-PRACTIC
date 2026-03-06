95eeecfaeffd4e06877cfefe69d94e1f53ee06ed
morokat85 <morokat85>
2026-03-06 22:24:17 +0700

    exercise

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -1,25 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
   /* You will need to use many state to keep the inut values and other needs */
 
+  const [numA, setNumA] = useState('');
+  const [numB, setNumB] = useState('');
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
+  
   /* You will need some function to handle the key pressed and button events */
+  const handleNumAChange = (event) => {
+    setNumA(event.target.value);
+    setReault('');
+    setError('');
+  }
+  const handleNumBChange = (event) => {
+    setNumB(event.target.value);
+    setReault('');
+    setError('');
+  }
+
+const handleCompute = () => {
+  const a = Number(numA);
+  const b = Number(numB);
+
+  if (isNaN(a) || isNaN(b)) {
+    setError("Please enter valid numbers");
+    setResult('');
+  } else {
+    setResult(a + b);
+    setError('');
+  }
+};
+  
 
   return (
     <main>
       <h1>Calculator</h1>
 
       <label>A =</label>
-      <input onKeyUp={onA} />
+      <input value={numA} onChange={handleNumAChange}/>
 
       <label>B =</label>
-      <input onKeyUp={onB} />
+      <input value={numB} onChange={handleNumBChange} />
 
       <label>A + B =</label>
 
       {/* When Compute buton is clicked, this input display the sum of the 2 numbers, or the error message in RED */}
-      <input disabled />
-      <button>Compute</button>
+      <input disabled value={error || result} style={{color: error ? 'red' : 'white'}}/>
+      <button onClick={handleCompute}>Compute</button>
     </main>
   );
 }
--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -1,21 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ images }) => {
   /* You will need to  use  state to mnage the current image */
+  const [current, setCurrent] = useState(0);
 
   /* You will need to hanle the click on left and right button */
+  const nextSlide = () => {
+    if (current < images.length - 1) {
+      setCurrent(current + 1);
+    }
+  };
 
+  const preSlide = () => {
+    if (current > 0) {
+      setCurrent(current - 1);
+    }
+  };
   /* You will need to manage the cases when we are on the last image or first image*/
 
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={preSlide} />
 
       {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      <img src={images[current].src} alt={images[current].alt} className="slide" />
 
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill className="arrow arrow-right" onClick={nextSlide}/>
     </div>
   );
 };
